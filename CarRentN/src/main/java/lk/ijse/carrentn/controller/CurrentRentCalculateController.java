24c7c1354fcee3b01b38cddb3034e977f046e1b3
sachin <sachinishanga91@gmail.com>
2025-12-21 11:36:55 +0530

    init Commit

--- CarRentN/src/main/java/lk/ijse/carrentn/controller/CurrentRentCalculateController.java
+++ CarRentN/src/main/java/lk/ijse/carrentn/controller/CurrentRentCalculateController.java
@@ -1,4 +1,113 @@
 package lk.ijse.carrentn.controller;
 
-public class CurrentRentCalculateController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CurrentRentCalculateController implements Initializable {
+
+    @FXML
+    private Label balanceLable;
+
+    @FXML
+    private Label basePayField;
+
+    @FXML
+    private TextField customeNICField;
+
+    @FXML
+    private ComboBox<?> customerCbox;
+
+    @FXML
+    private TextField customerNameField;
+
+    @FXML
+    private TextField customerPaidAmountField;
+
+    @FXML
+    private Label daysLable;
+
+    @FXML
+    private Label discountLable;
+
+    @FXML
+    private Label driverNameField;
+
+    @FXML
+    private Label driverRateField;
+
+    @FXML
+    private Label driverTotalLable;
+
+    @FXML
+    private Label eDateLable;
+
+    @FXML
+    private TextField invoiceIDField;
+
+    @FXML
+    private Label lateDatesLable;
+
+    @FXML
+    private Label lateFeeLable;
+
+    @FXML
+    private Label modelLable;
+
+    @FXML
+    private Label remainAmountLable;
+
+    @FXML
+    private Label returnedDateLable;
+
+    @FXML
+    private Label sDateLable;
+
+    @FXML
+    private Label selectCustomerLable;
+
+    @FXML
+    private TextField totalField;
+
+    @FXML
+    private Label vehicleRateLable;
+
+    @FXML
+    private Label vehicleTotalLable;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {}
+
+    @FXML
+    void handleComfirmPay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReset(ActionEvent event) {
+
+    }
+    @FXML
+    void handleSelectCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSerchRentByCustomerName(KeyEvent event) {
+
+    }
+
+    @FXML
+    void handleSerchRentByInvoiceId(KeyEvent event) {
+
+    }
+
+
 }
