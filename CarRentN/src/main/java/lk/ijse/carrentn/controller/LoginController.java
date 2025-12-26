2178e3130b19c99e1285088868bf32786992ce0a
sachin <sachinishanga91@gmail.com>
2025-12-26 13:21:32 +0530

    init commit

--- CarRentN/src/main/java/lk/ijse/carrentn/controller/LoginController.java
+++ CarRentN/src/main/java/lk/ijse/carrentn/controller/LoginController.java
@@ -1,39 +1,59 @@
 package lk.ijse.carrentn.controller;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import lk.ijse.carrentn.App;
+import lk.ijse.carrentn.dto.UserDTO;
+import lk.ijse.carrentn.model.UserModel;
 
 public class LoginController {
     @FXML
     private TextField userNameField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private TextField passwordShowField;
+    @FXML
+    private CheckBox checkBox;
+
+    UserModel userModel = new UserModel();
     
     
     @FXML
         private void login() throws Exception {
+
+        UserDTO userDTO = userModel.getUserDetails();
+
+        String uName = userNameField.getText().trim();
+        String pWord = passwordField.getText().trim();
+        String sPWORD = passwordShowField.getText().trim();
         
-        String realUserName = "1";
-        String realPassword = "1";
-        
-        String uName = userNameField.getText();
-        String pWord = passwordField.getText();
-        
-        if(uName.equals(realUserName) && pWord.equals(realPassword)){
+        if((uName.equals(userDTO.getUsername()) && pWord.equals(userDTO.getPassword()))||(uName.equals(userDTO.getUsername()) && sPWORD.equals(userDTO.getPassword()))){
             System.out.println("Logged-in Successfully!");
             App.setRoot("MainDashboard");
             
         } else {
             System.out.println("Invalid User Name or Password!");
-            
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Login Massage");
             alert.setHeaderText("Invalid user name or password");
             alert.show();
         }
+    }
+
+    @FXML
+    void changeVisibility() {
+        if (checkBox.isSelected()){
+            passwordShowField.setText(passwordField.getText());
+            passwordShowField.setVisible(true);
+            passwordField.setVisible(false);
+            return;
+        }
+        passwordField.setText(passwordShowField.getText());
+        passwordField.setVisible(true);
+        passwordShowField.setVisible(false);
     }
 }
